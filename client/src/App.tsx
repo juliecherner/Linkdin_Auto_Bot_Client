8b31739184567d99018af6f78a8fab3d05d12ea3
Yevgeny-Sh <yevgst7@gmail.com>
2022-03-14 16:15:32 +0200

    la6-basic-routs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 import MainPage from "./screens/mainPage/MainPage";
+import Login from "./screens/login/Login";
+import Logout from "./screens/logout/Logout";
+import ScrapingStrategies from "./screens/scraping-strategies/ScrapingStrategies";
+import About from "./screens/about/About";
+import NotFound from "./screens/not-found/NotFound";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <MainPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />}></Route>
+        <Route path="about" element={<MainPage />}></Route>
+        <Route path="login" element={<Login />}></Route>
+        <Route path="logout" element={<Logout />}></Route>
+        <Route
+          path="scraping-strategies"
+          element={<ScrapingStrategies />}
+        ></Route>
+        <Route path="about" element={<About />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
