73fa080f0728ddd953c33e4da07b6fc58747c0a1
Liran-tal <taliran44@gmail.com>
2022-03-14 23:29:32 +0200

    LA-31: add delete-profile controller and service

--- backEnd/src/controllers/profile.controller.ts
+++ backEnd/src/controllers/profile.controller.ts
@@ -5,10 +5,11 @@ import * as services from '../services/profile.service';
 
 export async function addProfile(
   req: Request<{}, {}, ProfileDocument>,
-  res: Response) {
+  res: Response
+) {
   try {
     const result = await services.addProfile(req.body);
-    
+
     return res.send("Profile insertion to DB success");
   } catch (error) {
     return res.status(500).send("Profile insertion to DB failed");
@@ -21,5 +22,21 @@ export async function getAllProfiles(req: Request, res: Response) {
     return res.send(profiles);
   } catch (error) {
     return res.status(500).send(getErrorMessage(error));
+  }
+}
+
+
+
+export async function deleteProfile(
+  req: Request<{}, {}, {}, ProfileDocument>,
+  res: Response
+) {
+  try {
+    const { _id } = req.query;
+    const result = await services.deleteProfile({ _id });
+
+    return res.send("Profile deletion from DB success");
+  } catch (error) {
+    return res.status(500).send("Profile deletion from DB failed");
   }
 }
--- backEnd/src/services/profile.service.ts
+++ backEnd/src/services/profile.service.ts
@@ -15,5 +15,13 @@ export async function addProfile(
 }
 
 export async function getAllProfiles() {
-  return ProfileModel.find({});
+  return ProfileModel.find({}).lean();
+}
+
+
+
+export async function deleteProfile(
+  query: FilterQuery<ProfileDocument>,
+  options: QueryOptions = { projection: 'email' }) {
+  return ProfileModel.findOneAndRemove(query, options);
 }
