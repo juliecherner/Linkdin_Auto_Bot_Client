b9d30644c96347a7a13636181dbfe1b72b3c7a17
Liran-tal <taliran44@gmail.com>
2022-03-14 17:41:00 +0200

    LA-30: add post route for profiles data including mongoDB

--- backEnd/src/controllers/profile.controller.ts
+++ backEnd/src/controllers/profile.controller.ts
@@ -1,7 +1,19 @@
 import { Request, Response } from 'express';
+import { ProfileDocument } from '../models/profile.model'
+import * as services from '../services/profile.service';
 
-
+export async function addProfile(
+  req: Request<{}, {}, ProfileDocument>,
+  res: Response) {
+  try {
+    const result = await services.addProfile(req.body);
+    
+    return res.send("Profile insertion to DB success");
+  } catch (error) {
+    return res.status(500).send("Profile insertion to DB failed");
+  }
+}
 
 export async function getAllProfiles(req: Request, res: Response) {
-  
+
 }
