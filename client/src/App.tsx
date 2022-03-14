b211e78f0faabf953e70020d99c7536793066b19
Yevgeny-Sh <yevgst7@gmail.com>
2022-03-14 11:00:54 +0200

    favicon & rect router install

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import MainPage from "./screens/mainPage/MainPage"
-import './App.css';
-
+import React from "react";
+import MainPage from "./screens/mainPage/MainPage";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <MainPage/>
+      <MainPage />
     </div>
   );
 }
