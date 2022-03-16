2bcdab013e3ec39d16643063d9f4685335e9e688
julie-chernen <juliechernen@gmail.com>
2022-03-16 09:32:31 +0200

    pages names refactored

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./screens/mainPage/MainPage";
 import LoginPage from "./screens/loginPage/LoginPage";
-import ScrapingStrategies from "./screens/StrategiesPage/StrategiesPage";
+import StrategiesPage from "./screens/StrategiesPage/StrategiesPage";
 import AboutPage from "./screens/aboutPage/AboutPage";
-import NotFound from "./screens/notFoundPage/NotFoundPage";
+import NotFoundPage from "./screens/notFoundPage/NotFoundPage";
 import "./App.css";
 
 const App: React.FC = () => {
@@ -16,10 +16,10 @@ const App: React.FC = () => {
         <Route path="login" element={<LoginPage />}></Route>
         <Route
           path="scraping-strategies"
-          element={<ScrapingStrategies />}
+          element={<StrategiesPage/>}
         ></Route>
         <Route path="about" element={<AboutPage />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
+        <Route path="*" element={<NotFoundPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
