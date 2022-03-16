bbddac5d47fbdfafa4206c4e50d27a4e6f27441c
julie-chernen <juliechernen@gmail.com>
2022-03-16 14:24:44 +0200

    api route changed

--- client/src/api/Api.ts
+++ client/src/api/Api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-let baseUrl =
-  process.env.NODE_ENV === "production" ? "/" : "http://localhost:8080/";
+let baseUrl = "https://linkdin-auto-bot-back.herokuapp.com"
+//   process.env.NODE_ENV === "production" ? "/" : "http://localhost:8080/";
 
 export default axios.create({
   baseURL: baseUrl,
