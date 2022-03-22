b96b263d15d6cfec7a95da66fadebe752d4a69e0
julie-chernen <juliechernen@gmail.com>
2022-03-22 14:59:30 +0200

    api request function is implemented

--- src/screens/mainPage/MainPage.tsx
+++ src/screens/mainPage/MainPage.tsx
@@ -1,42 +1,45 @@
-import {useEffect, useContext} from "react"
-import CardList from "./components/cardList/CardList"
-import Togglers from "./components/togglers/Togglers"
-import ImageArea from "./components/imageArea/imageArea"
-import Api from "../../api/Api"
-import {ProfileContext} from "../../context/profile.context"
+import { useEffect, useContext } from "react";
+import CardList from "./components/cardList/CardList";
+import Togglers from "./components/togglers/Togglers";
+import ImageArea from "./components/imageArea/imageArea";
+import { ProfileContext } from "../../context/profile.context";
+import { getProfiles } from "../../api/profile.api";
 
 const MainPage: React.FC = () => {
+  const { setProfiles, mode } = useContext(ProfileContext);
 
-const {setProfiles, mode} = useContext(ProfileContext)
+  useEffect(() => {
+    const trackMode = async () => {
+      switch (mode) {
+        case "Relevant":
+          const relevant = await getProfiles();
+          setProfiles(relevant);
+          break;
+        case "Latest":
+          //add sort
+          const latest = await getProfiles();
+          setProfiles(latest);
+          break;
+        case "Stared":
+          //add filter
+          const stared = await getProfiles();
+          setProfiles(stared);
+          break;
+        default:
+          const defaultData = await getProfiles();
+          setProfiles(defaultData);
+      }
+    };
+    trackMode();
+  }, [mode, setProfiles]);
 
-    useEffect(() => {
-        const trackMode = async()=>{
-        switch (mode){
-            case "Relevant": 
-            const relevant = await Api.get("/api/profile/profiles")
-            setProfiles(relevant.data) 
-           break;
-            case "Latest": 
-            const latest = await Api.get("/api/profile/profiles")
-            setProfiles(latest.data) 
-                break;
-            case "Stared":
-                const stared = await Api.get("/api/profile/profiles")
-                setProfiles(stared.data)
-                break;
-            default: 
-            const defaultData = await Api.get("/api/profile/profiles")
-            setProfiles(defaultData.data) 
-        }}
-        trackMode()
-    }, [mode, setProfiles])
+  return (
+    <div className="main-page">
+      <Togglers />
+      <ImageArea />
+      <CardList />
+    </div>
+  );
+};
 
-return (
-        <div className="main-page">
-            <Togglers/>
-            <ImageArea/>
-            <CardList/>
-        </div>)
-}
-
-export default MainPage
+export default MainPage;
