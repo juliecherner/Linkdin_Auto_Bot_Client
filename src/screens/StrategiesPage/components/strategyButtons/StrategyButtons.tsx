a5d473b3f3a74e64bef36c940433745e6ca88930
julie-chernen <juliechernen@gmail.com>
2022-04-12 23:47:43 +0300

    fakedata is deleted, save-strategy-button is deleted

--- src/screens/StrategiesPage/components/strategyButtons/StrategyButtons.tsx
+++ src/screens/StrategiesPage/components/strategyButtons/StrategyButtons.tsx
@@ -1,19 +1,11 @@
 import React from "react";
 import Button from "@mui/material/Button";
-import ButtonGroup from "@mui/material/ButtonGroup";
 
 const StrategyButtons: React.FC = () => {
   return (
-    <div>
-      <ButtonGroup variant="outlined" aria-label="outlined button group">
-        <Button variant="contained" color="success">
-          Save changes
-        </Button>
-        <Button variant="outlined" color="error">
-          Scrape now
-        </Button>
-      </ButtonGroup>
-    </div>
+    <Button variant="outlined" color="success">
+      Scrape now
+    </Button>
   );
 };
 
